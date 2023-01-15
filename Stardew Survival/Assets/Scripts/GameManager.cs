76a9c7ba8ecbd7ebfdaacdb0270ab3641b1bcb85
AlnCA <alnca0103@gmail.com>
2023-01-15 21:18:04 +0900

    Update GameManager.cs

--- Stardew Survival/Assets/Scripts/GameManager.cs
+++ Stardew Survival/Assets/Scripts/GameManager.cs
@@ -8,13 +8,12 @@ public class GameManager : MonoBehaviour
 
     public float silmang2ya;
     public float silmang2ya2;
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
