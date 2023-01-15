08b58989dd629d0b6dd4190db2018dd9be25148a
hyunSeo <bongbong107@gmail.com>
2023-01-15 21:15:55 +0900

    Update GameManager.cs

--- Stardew Survival/Assets/Scripts/GameManager.cs
+++ Stardew Survival/Assets/Scripts/GameManager.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-<<<<<<< HEAD
     public int abc;
 
-=======
     public float silmang2ya;
     public float silmang2ya2;
->>>>>>> 64a68c4ee93957f71d061ede52c8b9d4b0a99bcf
     // Start is called before the first frame update
     void Start()
     {
