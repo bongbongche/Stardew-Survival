767841ef60889fa63e7f8c8e222f8b084751b0e0
hyunSeo <bongbong107@gmail.com>
2023-01-16 21:23:29 +0900

    Farm Selection Finish

--- Stardew Survival/Assets/Scripts/MouseSelect.cs
+++ Stardew Survival/Assets/Scripts/MouseSelect.cs
@@ -5,12 +5,12 @@ using UnityEngine.Tilemaps;
 
 public class MouseSelect : MonoBehaviour
 {
-    // 타일맵 좌표 얻기 시도 중
-    public GridLayout grid;
-    public Tilemap farmTile;
-    public TileBase changeTile;
 
-    private Vector3Int cellPosition;
+    public GridLayout grid;
+    public Tilemap farmTile;    // 농장 타일
+    public TileBase changeTile; // 바꿀 타일
+
+    private Vector3Int cellPosition;    // 타일의 위치
     private Vector3 mousePosition;
     private Vector3 offset = new Vector3(0.5f, 0.5f, 0);
     // Start is called before the first frame update
@@ -22,13 +22,23 @@ public class MouseSelect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 마우스 포지션을 월드 포지션으로 변경 (= 게임 플레이 화면의 마우스 위치로 변경)
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // 마우스 포지션을 셀 포지션으로 변경
         cellPosition = grid.WorldToCell(mousePosition);
+        // select 오브젝트가 셀 포지션을 따라다니도록
         transform.position = cellPosition + offset;
 
-        if(Input.GetMouseButtonDown(0))
+        Plant();
+    }
+
+    // 식물 심기
+    private void Plant()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            if(farmTile.GetTile(cellPosition).name == "Desert _6")
+            // 농장 타일 위에서만 식물 심기 가능
+            if (farmTile.GetTile(cellPosition) != null)
             {
                 farmTile.SetTile(cellPosition, changeTile);
             }
