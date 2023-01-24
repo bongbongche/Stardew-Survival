7c583b00bbcda43b22c014387e8743810096e278
AlnCA <alnca0103@gmail.com>
2023-01-25 04:26:44 +0900

    0125 2

--- Stardew Survival/Assets/Scripts/HPBar/NigttTimer.cs
+++ Stardew Survival/Assets/Scripts/HPBar/NigttTimer.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 
 public class NigttTimer : MonoBehaviour
 {
-    public Slider hpbar;
+    public Slider nightTimerBar;
     public GameManager gameManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        hpbar.value = gameManager.nightTimer / gameManager.nightTime;
+        nightTimerBar.value = gameManager.nightTimer / gameManager.nightTime;
     }
 }
